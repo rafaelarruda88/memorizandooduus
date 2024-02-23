90f98166cf2c6ec0d17500448414f5f6b78f0abb
rafaelarruda88 <160373973+rafaelarruda88@users.noreply.github.com>
2024-02-23 18:36:24 -0300

    Update game.js
    256 Odús

--- game.js
+++ game.js
@@ -1,20 +1,261 @@
 const images = [
-    {src: 'image1.png', name: 'Eji Ogbe'},
-    {src: 'image2.png', name: 'Oyeku Meji'},
-    {src: 'image3.png', name: 'Iwori Meji'},
-    {src: 'image4.png', name: 'Odi Meji'},
-    {src: 'image5.png', name: 'Irosun Meji'},
-    {src: 'image6.png', name: 'Owonrin Meji'},
-    {src: 'image7.png', name: 'Obara Meji'},
-    {src: 'image8.png', name: 'Okanran Meji'},
-    {src: 'image9.png', name: 'Ogunda Meji'},
-    {src: 'image10.png', name: 'Osa Meji'},
-    {src: 'image11.png', name: 'Ika Meji'},
-    {src: 'image12.png', name: 'Oturupon Meji'},
-    {src: 'image13.png', name: 'Otura Meji'},
-    {src: 'image14.png', name: 'Irete Meji'},
-    {src: 'image15.png', name: 'Ose Meji'},
-    {src: 'image16.png', name: 'Ofun Meji'},
+
+    {src: 'Ika Irete.png, name: 'Ika Irete'},
+    {src: 'Ika Irosun.png, name: 'Ika Irosun'},
+    {src: 'Ika Iwori.png, name: 'Ika Iwori'},
+    {src: 'Ika Meji.png, name: 'Ika Meji'},
+    {src: 'Ika Obara.png, name: 'Ika Obara'},
+    {src: 'Ika Odí.png, name: 'Ika Odí'},
+    {src: 'Ika Ofun.png, name: 'Ika Ofun'},
+    {src: 'Ika Ogbe.png, name: 'Ika Ogbe'},
+    {src: 'Ika Ogunda.png, name: 'Ika Ogunda'},
+    {src: 'Ika Okanran.png, name: 'Ika Okanran'},
+    {src: 'Ika Osa.png, name: 'Ika Osa'},
+    {src: 'Ika Ose.png, name: 'Ika Ose'},
+    {src: 'Ika Otura.png, name: 'Ika Otura'},
+    {src: 'Ika Oturupon.png, name: 'Ika Oturupon'},
+    {src: 'Ika Owonrin.png, name: 'Ika Owonrin'},
+    {src: 'Ika Oyeku.png, name: 'Ika Oyeku'},
+    {src: 'Irete Ika.png, name: 'Irete Ika'},
+    {src: 'Irete Irosun.png, name: 'Irete Irosun'},
+    {src: 'Irete Iwori.png, name: 'Irete Iwori'},
+    {src: 'Irete Meji.png, name: 'Irete Meji'},
+    {src: 'Irete Obara.png, name: 'Irete Obara'},
+    {src: 'Irete Odí.png, name: 'Irete Odí'},
+    {src: 'Irete Ofun.png, name: 'Irete Ofun'},
+    {src: 'Irete Ogbe.png, name: 'Irete Ogbe'},
+    {src: 'Irete Ogunda.png, name: 'Irete Ogunda'},
+    {src: 'Irete Okanran.png, name: 'Irete Okanran'},
+    {src: 'Irete Osa.png, name: 'Irete Osa'},
+    {src: 'Irete Ose.png, name: 'Irete Ose'},
+    {src: 'Irete Otura.png, name: 'Irete Otura'},
+    {src: 'Irete Oturupon.png, name: 'Irete Oturupon'},
+    {src: 'Irete Owonrin.png, name: 'Irete Owonrin'},
+    {src: 'Irete Oyeku.png, name: 'Irete Oyeku'},
+    {src: 'Irosun Ika.png, name: 'Irosun Ika'},
+    {src: 'Irosun Irete.png, name: 'Irosun Irete'},
+    {src: 'Irosun Iwori.png, name: 'Irosun Iwori'},
+    {src: 'Irosun Meji.png, name: 'Irosun Meji'},
+    {src: 'Irosun Obara.png, name: 'Irosun Obara'},
+    {src: 'Irosun Odí.png, name: 'Irosun Odí'},
+    {src: 'Irosun Ofun.png, name: 'Irosun Ofun'},
+    {src: 'Irosun Ogbe.png, name: 'Irosun Ogbe'},
+    {src: 'Irosun Ogunda.png, name: 'Irosun Ogunda'},
+    {src: 'Irosun Okanran.png, name: 'Irosun Okanran'},
+    {src: 'Irosun Osa.png, name: 'Irosun Osa'},
+    {src: 'Irosun Ose.png, name: 'Irosun Ose'},
+    {src: 'Irosun Otura.png, name: 'Irosun Otura'},
+    {src: 'Irosun Oturupon.png, name: 'Irosun Oturupon'},
+    {src: 'Irosun Owonrin.png, name: 'Irosun Owonrin'},
+    {src: 'Irosun Oyeku.png, name: 'Irosun Oyeku'},
+    {src: 'Iwori Ika.png, name: 'Iwori Ika'},
+    {src: 'Iwori Irete.png, name: 'Iwori Irete'},
+    {src: 'Iwori Irosun.png, name: 'Iwori Irosun'},
+    {src: 'Iwori Meji.png, name: 'Iwori Meji'},
+    {src: 'Iwori Obara.png, name: 'Iwori Obara'},
+    {src: 'Iwori Odí.png, name: 'Iwori Odí'},
+    {src: 'Iwori Ofun.png, name: 'Iwori Ofun'},
+    {src: 'Iwori Ogbe.png, name: 'Iwori Ogbe'},
+    {src: 'Iwori Ogunda.png, name: 'Iwori Ogunda'},
+    {src: 'Iwori Okanran.png, name: 'Iwori Okanran'},
+    {src: 'Iwori Osa.png, name: 'Iwori Osa'},
+    {src: 'Iwori Ose.png, name: 'Iwori Ose'},
+    {src: 'Iwori Otura.png, name: 'Iwori Otura'},
+    {src: 'Iwori Oturupon.png, name: 'Iwori Oturupon'},
+    {src: 'Iwori Owonrin.png, name: 'Iwori Owonrin'},
+    {src: 'Iwori Oyeku.png, name: 'Iwori Oyeku'},
+    {src: 'Obara Ika.png, name: 'Obara Ika'},
+    {src: 'Obara Irete.png, name: 'Obara Irete'},
+    {src: 'Obara Irosun.png, name: 'Obara Irosun'},
+    {src: 'Obara Iwori.png, name: 'Obara Iwori'},
+    {src: 'Obara Meji.png, name: 'Obara Meji'},
+    {src: 'Obara Odí.png, name: 'Obara Odí'},
+    {src: 'Obara Ofun.png, name: 'Obara Ofun'},
+    {src: 'Obara Ogbe.png, name: 'Obara Ogbe'},
+    {src: 'Obara Ogunda.png, name: 'Obara Ogunda'},
+    {src: 'Obara Okanran.png, name: 'Obara Okanran'},
+    {src: 'Obara Osa.png, name: 'Obara Osa'},
+    {src: 'Obara Ose.png, name: 'Obara Ose'},
+    {src: 'Obara Otura.png, name: 'Obara Otura'},
+    {src: 'Obara Oturupon.png, name: 'Obara Oturupon'},
+    {src: 'Obara Owonrin.png, name: 'Obara Owonrin'},
+    {src: 'Obara Oyeku.png, name: 'Obara Oyeku'},
+    {src: 'Odí Ika.png, name: 'Odí Ika'},
+    {src: 'Odí Irete.png, name: 'Odí Irete'},
+    {src: 'Odí Irosun.png, name: 'Odí Irosun'},
+    {src: 'Odí Iwori.png, name: 'Odí Iwori'},
+    {src: 'Odí Meji.png, name: 'Odí Meji'},
+    {src: 'Odí Obara.png, name: 'Odí Obara'},
+    {src: 'Odí Ofun.png, name: 'Odí Ofun'},
+    {src: 'Odí Ogbe.png, name: 'Odí Ogbe'},
+    {src: 'Odí Ogunda.png, name: 'Odí Ogunda'},
+    {src: 'Odí Okanran.png, name: 'Odí Okanran'},
+    {src: 'Odí Osa.png, name: 'Odí Osa'},
+    {src: 'Odí Ose.png, name: 'Odí Ose'},
+    {src: 'Odí Otura.png, name: 'Odí Otura'},
+    {src: 'Odí Oturupon.png, name: 'Odí Oturupon'},
+    {src: 'Odí Owonrin.png, name: 'Odí Owonrin'},
+    {src: 'Odí Oyeku.png, name: 'Odí Oyeku'},
+    {src: 'Ofun Ika.png, name: 'Ofun Ika'},
+    {src: 'Ofun Irete.png, name: 'Ofun Irete'},
+    {src: 'Ofun Irosun.png, name: 'Ofun Irosun'},
+    {src: 'Ofun Iwori.png, name: 'Ofun Iwori'},
+    {src: 'Ofun Meji.png, name: 'Ofun Meji'},
+    {src: 'Ofun Obara.png, name: 'Ofun Obara'},
+    {src: 'Ofun Odí.png, name: 'Ofun Odí'},
+    {src: 'Ofun Ogbe.png, name: 'Ofun Ogbe'},
+    {src: 'Ofun Ogunda.png, name: 'Ofun Ogunda'},
+    {src: 'Ofun Okanran.png, name: 'Ofun Okanran'},
+    {src: 'Ofun Osa.png, name: 'Ofun Osa'},
+    {src: 'Ofun Ose.png, name: 'Ofun Ose'},
+    {src: 'Ofun Otura.png, name: 'Ofun Otura'},
+    {src: 'Ofun Oturupon.png, name: 'Ofun Oturupon'},
+    {src: 'Ofun Owonrin.png, name: 'Ofun Owonrin'},
+    {src: 'Ofun Oyeku.png, name: 'Ofun Oyeku'},
+    {src: 'Ogbe Ika.png, name: 'Ogbe Ika'},
+    {src: 'Ogbe Irete.png, name: 'Ogbe Irete'},
+    {src: 'Ogbe Irosun.png, name: 'Ogbe Irosun'},
+    {src: 'Ogbe Iwori.png, name: 'Ogbe Iwori'},
+    {src: 'Ogbe Meji.png, name: 'Ogbe Meji'},
+    {src: 'Ogbe Obara.png, name: 'Ogbe Obara'},
+    {src: 'Ogbe Odí.png, name: 'Ogbe Odí'},
+    {src: 'Ogbe Ofun.png, name: 'Ogbe Ofun'},
+    {src: 'Ogbe Ogunda.png, name: 'Ogbe Ogunda'},
+    {src: 'Ogbe Okanran.png, name: 'Ogbe Okanran'},
+    {src: 'Ogbe Osa.png, name: 'Ogbe Osa'},
+    {src: 'Ogbe Ose.png, name: 'Ogbe Ose'},
+    {src: 'Ogbe Otura.png, name: 'Ogbe Otura'},
+    {src: 'Ogbe Oturupon.png, name: 'Ogbe Oturupon'},
+    {src: 'Ogbe Owonrin.png, name: 'Ogbe Owonrin'},
+    {src: 'Ogbe Oyeku.png, name: 'Ogbe Oyeku'},
+    {src: 'Ogunda Ika.png, name: 'Ogunda Ika'},
+    {src: 'Ogunda Irete.png, name: 'Ogunda Irete'},
+    {src: 'Ogunda Irosun.png, name: 'Ogunda Irosun'},
+    {src: 'Ogunda Iwori.png, name: 'Ogunda Iwori'},
+    {src: 'Ogunda Meji.png, name: 'Ogunda Meji'},
+    {src: 'Ogunda Obara.png, name: 'Ogunda Obara'},
+    {src: 'Ogunda Odí.png, name: 'Ogunda Odí'},
+    {src: 'Ogunda Ofun.png, name: 'Ogunda Ofun'},
+    {src: 'Ogunda Ogbe.png, name: 'Ogunda Ogbe'},
+    {src: 'Ogunda Okanran.png, name: 'Ogunda Okanran'},
+    {src: 'Ogunda Osa.png, name: 'Ogunda Osa'},
+    {src: 'Ogunda Ose.png, name: 'Ogunda Ose'},
+    {src: 'Ogunda Otura.png, name: 'Ogunda Otura'},
+    {src: 'Ogunda Oturupon.png, name: 'Ogunda Oturupon'},
+    {src: 'Ogunda Owonrin.png, name: 'Ogunda Owonrin'},
+    {src: 'Ogunda Oyeku.png, name: 'Ogunda Oyeku'},
+    {src: 'Okanran Ika.png, name: 'Okanran Ika'},
+    {src: 'Okanran Irete.png, name: 'Okanran Irete'},
+    {src: 'Okanran Irosun.png, name: 'Okanran Irosun'},
+    {src: 'Okanran Iwori.png, name: 'Okanran Iwori'},
+    {src: 'Okanran Meji.png, name: 'Okanran Meji'},
+    {src: 'Okanran Obara.png, name: 'Okanran Obara'},
+    {src: 'Okanran Odí.png, name: 'Okanran Odí'},
+    {src: 'Okanran Ofun.png, name: 'Okanran Ofun'},
+    {src: 'Okanran Ogbe.png, name: 'Okanran Ogbe'},
+    {src: 'Okanran Ogunda.png, name: 'Okanran Ogunda'},
+    {src: 'Okanran Osa.png, name: 'Okanran Osa'},
+    {src: 'Okanran Ose.png, name: 'Okanran Ose'},
+    {src: 'Okanran Otura.png, name: 'Okanran Otura'},
+    {src: 'Okanran Oturupon.png, name: 'Okanran Oturupon'},
+    {src: 'Okanran Owonrin.png, name: 'Okanran Owonrin'},
+    {src: 'Okanran Oyeku.png, name: 'Okanran Oyeku'},
+    {src: 'Osa Ika.png, name: 'Osa Ika'},
+    {src: 'Osa Irete.png, name: 'Osa Irete'},
+    {src: 'Osa Irosun.png, name: 'Osa Irosun'},
+    {src: 'Osa Iwori.png, name: 'Osa Iwori'},
+    {src: 'Osa Meji.png, name: 'Osa Meji'},
+    {src: 'Osa Obara.png, name: 'Osa Obara'},
+    {src: 'Osa Odí.png, name: 'Osa Odí'},
+    {src: 'Osa Ofun.png, name: 'Osa Ofun'},
+    {src: 'Osa Ogbe.png, name: 'Osa Ogbe'},
+    {src: 'Osa Ogunda.png, name: 'Osa Ogunda'},
+    {src: 'Osa Okanran.png, name: 'Osa Okanran'},
+    {src: 'Osa Ose.png, name: 'Osa Ose'},
+    {src: 'Osa Otura.png, name: 'Osa Otura'},
+    {src: 'Osa Oturupon.png, name: 'Osa Oturupon'},
+    {src: 'Osa Owonrin.png, name: 'Osa Owonrin'},
+    {src: 'Osa Oyeku.png, name: 'Osa Oyeku'},
+    {src: 'Ose Ika.png, name: 'Ose Ika'},
+    {src: 'Ose Irete.png, name: 'Ose Irete'},
+    {src: 'Ose Irosun.png, name: 'Ose Irosun'},
+    {src: 'Ose Iwori.png, name: 'Ose Iwori'},
+    {src: 'Ose Meji.png, name: 'Ose Meji'},
+    {src: 'Ose Obara.png, name: 'Ose Obara'},
+    {src: 'Ose Odí.png, name: 'Ose Odí'},
+    {src: 'Ose Ofun.png, name: 'Ose Ofun'},
+    {src: 'Ose Ogbe Meji.png, name: 'Ose Ogbe Meji'},
+    {src: 'Ose Ogunda.png, name: 'Ose Ogunda'},
+    {src: 'Ose Okanran.png, name: 'Ose Okanran'},
+    {src: 'Ose Osa.png, name: 'Ose Osa'},
+    {src: 'Ose Otura.png, name: 'Ose Otura'},
+    {src: 'Ose Oturupon.png, name: 'Ose Oturupon'},
+    {src: 'Ose Owonrin.png, name: 'Ose Owonrin'},
+    {src: 'Ose Oyeku.png, name: 'Ose Oyeku'},
+    {src: 'Otura Ika.png, name: 'Otura Ika'},
+    {src: 'Otura Irete.png, name: 'Otura Irete'},
+    {src: 'Otura Irosun.png, name: 'Otura Irosun'},
+    {src: 'Otura Iwori.png, name: 'Otura Iwori'},
+    {src: 'Otura Meji.png, name: 'Otura Meji'},
+    {src: 'Otura Obara.png, name: 'Otura Obara'},
+    {src: 'Otura Odí.png, name: 'Otura Odí'},
+    {src: 'Otura Ofun.png, name: 'Otura Ofun'},
+    {src: 'Otura Ogbe.png, name: 'Otura Ogbe'},
+    {src: 'Otura Ogunda.png, name: 'Otura Ogunda'},
+    {src: 'Otura Okanran.png, name: 'Otura Okanran'},
+    {src: 'Otura Osa.png, name: 'Otura Osa'},
+    {src: 'Otura Ose.png, name: 'Otura Ose'},
+    {src: 'Otura Oturupon.png, name: 'Otura Oturupon'},
+    {src: 'Otura Owonrin.png, name: 'Otura Owonrin'},
+    {src: 'Otura Oyeku.png, name: 'Otura Oyeku'},
+    {src: 'Oturupon Ika.png, name: 'Oturupon Ika'},
+    {src: 'Oturupon Irete.png, name: 'Oturupon Irete'},
+    {src: 'Oturupon Irosun.png, name: 'Oturupon Irosun'},
+    {src: 'Oturupon Iwori.png, name: 'Oturupon Iwori'},
+    {src: 'Oturupon Meji.png, name: 'Oturupon Meji'},
+    {src: 'Oturupon Obara.png, name: 'Oturupon Obara'},
+    {src: 'Oturupon Odí.png, name: 'Oturupon Odí'},
+    {src: 'Oturupon Ofun.png, name: 'Oturupon Ofun'},
+    {src: 'Oturupon Ogbe.png, name: 'Oturupon Ogbe'},
+    {src: 'Oturupon Ogunda.png, name: 'Oturupon Ogunda'},
+    {src: 'Oturupon Okanran.png, name: 'Oturupon Okanran'},
+    {src: 'Oturupon Osa.png, name: 'Oturupon Osa'},
+    {src: 'Oturupon Ose.png, name: 'Oturupon Ose'},
+    {src: 'Oturupon Otura.png, name: 'Oturupon Otura'},
+    {src: 'Oturupon Owonrin.png, name: 'Oturupon Owonrin'},
+    {src: 'Oturupon Oyeku.png, name: 'Oturupon Oyeku'},
+    {src: 'Owonrin Ika.png, name: 'Owonrin Ika'},
+    {src: 'Owonrin Irete.png, name: 'Owonrin Irete'},
+    {src: 'Owonrin Irosun.png, name: 'Owonrin Irosun'},
+    {src: 'Owonrin Iwori.png, name: 'Owonrin Iwori'},
+    {src: 'Owonrin Meji.png, name: 'Owonrin Meji'},
+    {src: 'Owonrin Obara.png, name: 'Owonrin Obara'},
+    {src: 'Owonrin Odí.png, name: 'Owonrin Odí'},
+    {src: 'Owonrin Ofun.png, name: 'Owonrin Ofun'},
+    {src: 'Owonrin Ogbe.png, name: 'Owonrin Ogbe'},
+    {src: 'Owonrin Ogunda.png, name: 'Owonrin Ogunda'},
+    {src: 'Owonrin Okanran.png, name: 'Owonrin Okanran'},
+    {src: 'Owonrin Osa.png, name: 'Owonrin Osa'},
+    {src: 'Owonrin Ose.png, name: 'Owonrin Ose'},
+    {src: 'Owonrin Otura.png, name: 'Owonrin Otura'},
+    {src: 'Owonrin Oturupon.png, name: 'Owonrin Oturupon'},
+    {src: 'Owonrin Oyeku.png, name: 'Owonrin Oyeku'},
+    {src: 'Oyeku Ika.png, name: 'Oyeku Ika'},
+    {src: 'Oyeku Irete.png, name: 'Oyeku Irete'},
+    {src: 'Oyeku Irosun.png, name: 'Oyeku Irosun'},
+    {src: 'Oyeku Iwori.png, name: 'Oyeku Iwori'},
+    {src: 'Oyeku Meji.png, name: 'Oyeku Meji'},
+    {src: 'Oyeku Obara.png, name: 'Oyeku Obara'},
+    {src: 'Oyeku Odí.png, name: 'Oyeku Odí'},
+    {src: 'Oyeku Ofun.png, name: 'Oyeku Ofun'},
+    {src: 'Oyeku Ogbe.png, name: 'Oyeku Ogbe'},
+    {src: 'Oyeku Ogunda.png, name: 'Oyeku Ogunda'},
+    {src: 'Oyeku Okanran.png, name: 'Oyeku Okanran'},
+    {src: 'Oyeku Osa.png, name: 'Oyeku Osa'},
+    {src: 'Oyeku Ose.png, name: 'Oyeku Ose'},
+    {src: 'Oyeku Otura.png, name: 'Oyeku Otura'},
+    {src: 'Oyeku Oturupon.png, name: 'Oyeku Oturupon'},
+    {src: 'Oyeku Owonrin.png, name: 'Oyeku Owonrin'},
 ];
 
 let correctCount = 0;
@@ -25,32 +266,28 @@ function setupGame() {
     const optionsContainer = document.getElementById('options');
     const statusMessage = document.getElementById('statusMessage');
 
-    // Limpa opções anteriores e a mensagem de status
-    optionsContainer.innerHTML = '';
-    statusMessage.textContent = '';
-
-    // Embaralha as imagens para seleção aleatória
-    let shuffledImages = [...images].sort(() => 0.5 - Math.random());
-    let selectedImage = shuffledImages[0];
-    let options = [selectedImage];
-
-    // Escolhe mais duas opções aleatórias diferentes da correta
-    while (options.length < 3) {
-        let randomOption = shuffledImages[Math.floor(Math.random() * shuffledImages.length)];
-        if (!options.includes(randomOption)) {
-            options.push(randomOption);
-        }
-    }
-
-    // Embaralha as opções para não deixar a correta sempre na mesma posição
-    options.sort(() => 0.5 - Math.random());
-
-    // Atualiza a imagem a ser adivinhada
+    const shuffledImages = [...images].sort(() => 0.5 - Math.random());
+    const selectedImage = shuffledImages[0];
     imageElement.src = selectedImage.src;
     imageElement.alt = selectedImage.name;
 
-    // Cria botões para as opções de resposta
-    options.forEach(option => {
+    optionsContainer.innerHTML = '';
+    statusMessage.textContent = '';
+
+    // Seleciona mais duas imagens aleatórias para serem as opções erradas
+    let options = [selectedImage];
+    while (options.length < 3) {
+        let option = shuffledImages[Math.floor(Math.random() * shuffledImages.length)];
+        if (!options.includes(option)) {
+            options.push(option);
+        }
+    }
+
+    // Embaralha as opções para que a posição da resposta correta varie
+    options = options.sort(() => 0.5 - Math.random());
+
+    // Cria botões apenas para as três opções selecionadas
+    options.forEach((option) => {
         const button = document.createElement('button');
         button.textContent = option.name;
         button.onclick = () => checkAnswer(option.name, selectedImage.name);
@@ -66,12 +303,19 @@ function checkAnswer(selectedName, correctName) {
         wrongCount++;
         document.getElementById('statusMessage').textContent = 'Incorreto, tente novamente.';
     }
-    // Atualiza os contadores na tela
     document.getElementById('correctCount').textContent = correctCount;
     document.getElementById('wrongCount').textContent = wrongCount;
-    // Configura a próxima rodada
-    setTimeout(setupGame, 1000); // Dá um breve intervalo antes de iniciar a nova rodada
+    // Aguarda um pouco antes de configurar o próximo jogo para que o jogador veja a resposta
+    setTimeout(setupGame, 1000);
 }
 
-// Inicializa o jogo quando a janela é carregada
+function resetGame() {
+    correctCount = 0;
+    wrongCount = 0;
+    setupGame();
+    document.getElementById('correctCount').textContent = correctCount;
+    document.getElementById('wrongCount').textContent = wrongCount;
+    document.getElementById('statusMessage').textContent = '';
+}
+
 window.onload = setupGame;
